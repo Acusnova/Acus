3e58730c03460105688c4346e6b64243cac9d548
DavyPannenkoek <dv.visser@hotmail.com>
2014-12-09 10:10:24 +0100

    test
    test voor jim

---
+++ tc.js
@@ -0,0 +1,5 @@
+function tc () {
+  document.css('cursor','url(http://w17.snunit.k12.il/images/big_arrow.png),auto');
+};
+
+
---
+++ test_cursor.php
@@ -0,0 +1,28 @@
+<?php
+?>
+<link href="Acus.css" rel="stylesheet">	
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Acus</title>   
+    <link href="Bootstrap/css/bootstrap.css" rel="stylesheet">   
+    <link href="Login.css" rel="stylesheet"> 
+	<link href="Acus.css" rel="stylesheet">	
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+	<script src="Functies.js"></script>
+	<script src="tc.js"></script>
+	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+ 
+  </head>
+  <body>
+<div class="boxy">
+<button type="button" class="btn btn-primary test" id="bt" onclick="tc()" >Test</button>
+  </body>
+</div>
+</html>
